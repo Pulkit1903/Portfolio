d365080ceb380b8f80cd2db3955d459a29efdd27
Pulkit Heda <pulkit.19.2003@gmail.com>
2025-04-21 15:30:25 +0530

    Bucket Name Updated

--- Terraform_S3/main.tf
+++ Terraform_S3/main.tf
@@ -1,7 +1,7 @@
 module "my-portfolio"{
     source = "./AWS_S3"
     region = "us-east-02"
-    bucket_name = "Pulkit-Portfolio-S3-2025"
+    bucket_name = "pulkit-portfolio-s3-2025"
     host_file = "index.html"
     error_file = "error.html"
     version_boolean = false
